6e5f6469611d6024415db6f60253d01de5787515
pwnerblu <netcomusicinquiries@gmail.com>
2026-03-05 16:26:50 -0500

    update for usability?

--- ipad1-ios7.sh
+++ ipad1-ios7.sh
@@ -14,6 +14,9 @@ curl -L -o $SCRIPT_DIR/bins/irecovery https://github.com/NyanSatan/SundanceInH2A
 curl -L -o $SCRIPT_DIR/bins/idevicerestore https://github.com/NyanSatan/SundanceInH2A/raw/refs/heads/master/executables/idevicerestore
 curl -L -o $SCRIPT_DIR/bins/hfsplus https://github.com/NyanSatan/SundanceInH2A/raw/refs/heads/master/executables/hfsplus
 curl -L -o $SCRIPT_DIR/dtre/ddt.py https://github.com/NyanSatan/SundanceInH2A/raw/refs/heads/master/dt/ddt.py
+# These are for untethering it, thanks to NyanSatan
+curl -L -o $SCRIPT_DIR/bins/rc.boot https://github.com/NyanSatan/SundanceInH2A/raw/refs/heads/master/rc_boot/rc.boot
+curl -L -o $SCRIPT_DIR/bins/exploit.dmg https://github.com/NyanSatan/SundanceInH2A/raw/refs/heads/master/exploit/exploit-k48.dmg
 chmod +x $SCRIPT_DIR/bins/*
 xattr -cr $SCRIPT_DIR/bins/*
 
@@ -23,7 +26,7 @@ if [ "$#" -ne 3 ]; then
     echo "Usage: $0 [base ipsw] [target ipsw] [output name]"
     echo ""
     echo "Example:"
-    echo "$0 iPad1,1_5.1.1_9B206_Restore.ipsw iPad2,1_7.0_11A465_Restore.ipsw iPad1,1_7.0_11A465_Restore"
+    echo "$0 iPad1,1_5.1.1_9B206_Restore.ipsw iPhone3,1_7.0_11A465_Restore.ipsw iPad1,1_7.0_11A465_Restore"
     exit 1
 fi
 
@@ -44,8 +47,19 @@ $SCRIPT_DIR/bins/img3maker -f iBSS.patched -o $OUTPUT_NAME/Firmware/dfu/iBSS.k48
 $SCRIPT_DIR/bins/img3maker -f iBEC.patched -o $OUTPUT_NAME/Firmware/dfu/iBEC.k48ap.RELEASE.dfu -t ibec
 rm -rf iBSS.dec iBSS.patched iBEC.dec iBEC.patched
 echo "Extracting iOS 7 root filesystem"
-$SCRIPT_DIR/bins/dmg extract tmp/038-3423-394.dmg rootfs.raw -k 22c8a5554401cf466a2fdcf4f1156bd0b15bcf38d6b04356c3628f5405415debcb1f5061
-# todo, potential modifications?
+$SCRIPT_DIR/bins/dmg extract tmp/038-3447-395.dmg rootfs.raw -k 89d4dadced94577508999a1ce2a08b346328d9b25ad4e63b4220ce441cce35cf9e0a108b
+echo "Modifying iOS 7 root filesystem"
+$SCRIPT_DIR/bins/hfsplus rootfs.raw grow 2500000000
+# $SCRIPT_DIR/bins/hfsplus rootfs.raw untar $SCRIPT_DIR/resources/springboard.tar System/Library/CoreServices/SpringBoard.app/ might not be required atm
+# for touchscreen to work
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/Common.mtprops usr/share/firmware/multitouch/Common.mtprops
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/iPad.mtprops usr/share/firmware/multitouch/iPad.mtprops
+# WiFi drivers
+$SCRIPT_DIR/bins/hfsplus rootfs.raw mkdir usr/share/firmware/wifi/4329c0
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/duo.bin usr/share/firmware/wifi/4329c0/uno.bin
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/duo.txt usr/share/firmware/wifi/4329c0/uno.txt
+echo "Adding hacktivation"
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/MobileGestalt.plist private/var/mobile/Library/Caches/com.apple.MobileGestalt.plist
 echo "Building root filesystem"
 $SCRIPT_DIR/bins/dmg build rootfs.raw $OUTPUT_NAME/038-4291-006.dmg 
 rm -rf "rootfs.raw"
@@ -55,21 +69,27 @@ python3.11 $SCRIPT_DIR/dtre/ddt.py apply DeviceTree.raw DeviceTree.patch $SCRIPT
 $SCRIPT_DIR/bins/img3maker -f DeviceTree.patch -o $OUTPUT_NAME/Firmware/all_flash/all_flash.k48ap.production/DeviceTree.k48ap.img3 -t dtre
 rm -rf DeviceTree.raw DeviceTree.patch
 echo "Processing restore ramdisk"
-$SCRIPT_DIR/bins/xpwntool tmp/038-3465-251.dmg ramdisk.raw -iv 9589205905735ab88c7e129083bb0e3d -k f2acfe16974b1839d06148f1d3d165ce41c26f9accdd2cea90a4d3e4c5a2a02b
+$SCRIPT_DIR/bins/xpwntool tmp/038-3373-256.dmg ramdisk.raw -iv 076220ac2c46cd54f1eff12d78f044b2 -k b2101ec5cdd1919c5b0e6dd7116f576ec38b62d9d2880418486fa9f2237e94cc
 $SCRIPT_DIR/bins/hfsplus ramdisk.raw grow 20000000
 $SCRIPT_DIR/bins/hfsplus ramdisk.raw rm usr/sbin/asr
 $SCRIPT_DIR/bins/hfsplus ramdisk.raw add $SCRIPT_DIR/Ramdisk-stuff/asr usr/sbin/asr
 $SCRIPT_DIR/bins/hfsplus ramdisk.raw chmod 100755 usr/sbin/asr
-$SCRIPT_DIR/bins/hfsplus ramdisk.raw mv usr/local/share/restore/options.k93.plist usr/local/share/restore/options.k48.plist
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw extract usr/local/share/restore/options.n90.plist 
+# Add baseband update skip
+printf "<key>UpdateBaseband</key><false/>\n" >> options.n90.plist
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw rm usr/local/share/restore/options.n90.plist 
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw add options.n90.plist usr/local/share/restore/options.k48.plist
+echo "Adding untether stuff"
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw rm etc/rc.boot
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw add $SCRIPT_DIR/bins/rc.boot etc/rc.boot
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw chmod 100755 etc/rc.boot
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw add $SCRIPT_DIR/bins/exploit.dmg exploit.dmg
 $SCRIPT_DIR/bins/img3maker -f ramdisk.raw -o $OUTPUT_NAME/038-4361-021.dmg -t rdsk
-rm -rf ramdisk.raw
-echo "Downloading iPhone3,1 7.0 kernelcache"
-$SCRIPT_DIR/bins/pzb -g kernelcache.release.n90 https://secure-appldnld.apple.com/iOS7/091-9485.20130918.Xa98u/iPhone3,1_7.0_11A465_Restore.ipsw
+rm -rf ramdisk.raw options.n90.plist
 echo "Replacing kernelcache with iPhone3,1 7.0 kernelcache"
-mv kernelcache.release.n90 $OUTPUT_NAME/kernelcache.release.k48
+mv tmp/kernelcache.release.n90 $OUTPUT_NAME/kernelcache.release.k48
 rm -rf "tmp"
 echo "Finished"
-
 
 
 
