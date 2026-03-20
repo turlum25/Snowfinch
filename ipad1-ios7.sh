54b440b6f8997a208cb0fc3ae10227c20dbca9e1
pwnerblu <alexisjeanstansbury@gmail.com>
2026-03-19 20:45:27 -0400

    add touch4 iOS 7 support

--- ipad1-ios7.sh
+++ ipad1-ios7.sh
@@ -67,6 +67,7 @@ if [[ "$#" -lt 3 || "$#" -gt 4 ]]; then
     echo ""
     echo "Example:"
     echo "$0 iPad1,1_5.1.1_9B206_Restore.ipsw iPad2,1_7.0_11A465_Restore.ipsw iPad1,1_7.0_11A465_Restore [--hacktivate]"
+    echo "$0 iPod4,1_6.1.6_10B500_Restore.ipsw iPhone3,1_7.0_11A465_Restore.ipsw iPod4,1_7.0_11A465_Restore"
     exit 1
 fi
 
@@ -80,6 +81,7 @@ if [[ $# -eq 4 ]]; then
     if [[ "$4" == "--hacktivate" ]]; then
         echo "This will automatically hacktivate the restore"
         echo "It is only recommended for cellular models or if normal activation fails on Wi-Fi models."
+        echo "Hacktivation will NOT be enabled if you're making a custom 7.x IPSW for iPod touch 4"
         read -p "Press enter to continue with hacktivating the restore"
         HACKTIVATE=1
     else
@@ -88,10 +90,71 @@ if [[ $# -eq 4 ]]; then
     fi
 fi
 
+touch4_ios7_ipsw(){
+
+echo "Processing iBSS and iBEC"
+$SCRIPT_DIR/bins/xpwntool $OUTPUT_NAME/Firmware/dfu/iBSS.n81ap.RELEASE.dfu iBSS.dec -iv daafc6ddd42c8f807000b9c1dd453236 -k 1e68d69064ca17c6717be4fa4ff09a71eba1ad0af2a96df4b99a69f6e7258058
+$SCRIPT_DIR/bins/xpwntool $OUTPUT_NAME/Firmware/dfu/iBEC.n81ap.RELEASE.dfu iBEC.dec -iv fb44e5dbd3eb75d20f83c0f14d452346 -k 12a5192b4a2e860a76e9368e18e182e5f9f4809dcba62098fcbbaa63ef998a3c
+$SCRIPT_DIR/bins/iBoot32Patcher iBSS.dec iBSS.patched --rsa
+$SCRIPT_DIR/bins/iBoot32Patcher iBEC.dec iBEC.patched --rsa --debug -b "rd=md0 -v amfi=0xff amfi_get_out_of_my_way=1 cs_enforcement_disable=1 pio-error=0"
+$SCRIPT_DIR/bins/img3maker -f iBSS.patched -o $OUTPUT_NAME/Firmware/dfu/iBSS.n81ap.RELEASE.dfu -t ibss
+$SCRIPT_DIR/bins/img3maker -f iBEC.patched -o $OUTPUT_NAME/Firmware/dfu/iBEC.n81ap.RELEASE.dfu -t ibec
+rm -rf iBSS.dec iBSS.patched iBEC.dec iBEC.patched
+echo "Extracting iOS 7 root filesystem"
+$SCRIPT_DIR/bins/dmg extract tmp/038-3447-395.dmg rootfs.raw -k 89d4dadced94577508999a1ce2a08b346328d9b25ad4e63b4220ce441cce35cf9e0a108b
+echo "Modifying iOS 7 root filesystem"
+$SCRIPT_DIR/bins/hfsplus rootfs.raw grow 2500000000
+echo "Adding touch and multitouch drivers"
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/N81.mtprops usr/share/firmware/multitouch/N81.mtprops
+echo "Adding Wi-Fi drivers"
+$SCRIPT_DIR/bins/hfsplus rootfs.raw mkdir usr/share/firmware/wifi/4329c0
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/loco.bin usr/share/firmware/wifi/4329c0/uno.bin
+$SCRIPT_DIR/bins/hfsplus rootfs.raw add $SCRIPT_DIR/resources/loco.txt usr/share/firmware/wifi/4329c0/uno.txt
+echo "Building root filesystem"
+$SCRIPT_DIR/bins/dmg build rootfs.raw $OUTPUT_NAME/058-2543-001.dmg
+rm -rf "rootfs.raw"
+echo "Replacing DeviceTree"
+cp $SCRIPT_DIR/dtre/DeviceTree.n81ap.img3 $OUTPUT_NAME/Firmware/all_flash/all_flash.n81ap.production/DeviceTree.n81ap.img3 
+echo "Processing restore ramdisk"
+$SCRIPT_DIR/bins/xpwntool tmp/038-3373-256.dmg ramdisk.raw -iv 076220ac2c46cd54f1eff12d78f044b2 -k b2101ec5cdd1919c5b0e6dd7116f576ec38b62d9d2880418486fa9f2237e94cc
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw grow 20000000
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw rm usr/sbin/asr
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw add $SCRIPT_DIR/Ramdisk-stuff/asr usr/sbin/asr
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw chmod 100755 usr/sbin/asr
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw extract usr/local/share/restore/options.n90.plist 
+plutil -replace SystemPartitionSize -integer 3000 options.n90.plist
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw rm usr/local/share/restore/options.n90.plist 
+$SCRIPT_DIR/bins/hfsplus ramdisk.raw add options.n90.plist usr/local/share/restore/options.n81.plist
+$SCRIPT_DIR/bins/img3maker -f ramdisk.raw -o $OUTPUT_NAME/058-2540-001.dmg -t rdsk
+rm -rf ramdisk.raw options.n90.plist
+mv tmp/kernelcache.release.n90 $OUTPUT_NAME/kernelcache.release.n81
+rm -rf "tmp"
+echo "Finished making bundle"
+echo "You can now put the device into pwned DFU with ipwnder32, then restore the device with idevicerestore. Example: ./bins/idevicerestore -e $OUTPUT_NAME"
+echo "Assuming the restore succeeded, you can boot with ./boot2.sh"
+echo "Note that the device will not activate, so you will need to find a way to skip Setup. I won't be linking to any of these, so figure it out yourself."
+exit 0
+
+}
+
+
 echo "Extracting iOS 5 IPSW"
 unzip $BASE_IPSW -d $OUTPUT_NAME
 echo "Extracting iOS 7 IPSW"
 unzip $TARGET_IPSW -d tmp
+SHA_BUILDMANIFEST_BASE=$(shasum -a 256 "$OUTPUT_NAME/BuildManifest.plist" | awk '{print $1}')
+
+if [[ "$SHA_BUILDMANIFEST_BASE" == "cb63d1bc9df84eb756f9fdb3f8654a21d40b14e4c97265e65611b5c160db7cb7" ]]; then
+    DEVICE="iPad1,1"
+    echo "Continuing with iPad1,1 cfw making"
+    echo "Detected device: $DEVICE"
+else
+    DEVICE="iPod4,1"
+    echo "Detected device: $DEVICE"
+    touch4_ios7_ipsw
+    exit
+fi
+
 echo "Processing iBSS and iBEC"
 $SCRIPT_DIR/bins/xpwntool $OUTPUT_NAME/Firmware/dfu/iBSS.k48ap.RELEASE.dfu iBSS.dec -iv 9c69f81db931108e8efc268de3f5d94d -k 92f1cc2ca8362740734d69386fa6dde5582e18786777e1f9772d5dd364d873fb
 $SCRIPT_DIR/bins/xpwntool $OUTPUT_NAME/Firmware/dfu/iBEC.k48ap.RELEASE.dfu iBEC.dec -iv bde7b0d5cf7861479d81eb23f99d2e9e -k 1ba1f38e6a5b4841c1716c11acae9ee0fb471e50362a3b0dd8d98019f174a2f2
@@ -161,3 +224,4 @@ sleep 3
 # BY TURLUM25 - COMPILE SNOWFINCH RESTORE
 cd $SCRIPT_DIR/restore && clang Snowfinch.c -o snowfinch
 ./snowfinch
+sudo rm -rf "iPad1,1_7.0_11A465_Restore"
